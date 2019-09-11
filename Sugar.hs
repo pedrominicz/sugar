71ff0b60797d5962b1730ff59b8f5803babb9a64
Pedro Minicz <pedrominicz@gmail.com>
2019-09-10 21:07:50 -0300

    Add lambda expression parser.

--- Sugar.hs
+++ Sugar.hs
@@ -1,13 +1,65 @@
-module Sugar where
+module Sugar (Sugar(..)) where
 
-type Context = [(String, [Sugar] -> Sugar)]
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
 data Sugar
-  = Atom String
-  | List [Sugar]
-  | Number Integer
+  = Var String
+  | App Sugar Sugar
+  | Lam String Sugar
+  | Num Integer
 
 instance Show Sugar where
-  show (Atom x)   = x
-  show (List xs)  = "(" ++ unwords (map show xs) ++ ")"
-  show (Number x) = show x
+  show (Var x)   = x
+  show (App x y) = "(" ++ show x ++ " " ++ show y ++ ")"
+  show (Lam x y) = "(\\" ++ x ++ " -> " ++ show y ++ ")"
+  show (Num x)   = show x
+
+instance Read Sugar where
+  readsPrec _ =
+    \s -> case parse (whitespace *> sugar <* whitespace <* eof) "" s of
+      Left e  -> error $ show e
+      Right x -> [(x, "")]
+
+sugar :: Parser Sugar
+sugar = variable
+    <|> application
+    <|> lambda
+    <|> number
+
+variable :: Parser Sugar
+variable = Var . pure <$> oneOf ['a'..'z']
+
+application :: Parser Sugar
+application = parens $ do
+  x <- sugar
+  whitespace
+  y <- sugar
+  pure (App x y)
+
+lambda :: Parser Sugar
+lambda = parens $ do
+  char '\\' *> whitespace
+  x <- oneOf ['a'..'z']
+  whitespace *> string "->" *> whitespace
+  y <- sugar
+  pure (Lam (pure x) y)
+
+number :: Parser Sugar
+number = Num <$> try number'
+  where number' = do
+          sign   <- many (oneOf "+-")
+          digits <- many1 digit
+          pure $ read (sign ++ digits)
+
+parens :: Parser a -> Parser a
+parens p = try $ between open close (whitespace *> p <* whitespace)
+  where open  = char '('
+        close = char ')' <?> "')'"
+
+whitespace :: Parser ()
+whitespace = skipMany (skipMany1 space <|> comment <?> "")
+  where comment = do
+          _ <- try $ char '#'
+          skipMany (satisfy (/= '\n'))
+          pure ()
