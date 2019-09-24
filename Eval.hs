14c0a977ef2d02a4b40f4ff2a27fca1fb0986366
Pedro Minicz <pedrominicz@gmail.com>
2019-09-24 16:41:40 -0300

    Rewrite `eval` as an application based evaluator with support for `let` expressions.

--- Eval.hs
+++ Eval.hs
@@ -1,30 +1,39 @@
 module Eval
-  ( Environment
-  , eval
+  ( eval
   ) where
 
 import Expr
+import Value
 
+import Control.Monad.Except
 import Control.Monad.State
 
-type Environment = [(String, Expr)]
+import Safe (atMay)
 
-eval :: Expr -> StateT Environment Maybe Expr
-eval (Ref _)    = lift $ Nothing
-eval (Global x) = do
+eval :: Expr -> StateT Environment (Except String) Value
+eval = eval' []
+
+eval' :: [Value] -> Expr -> StateT Environment (Except String) Value
+eval' env (Ref x) =
+  case atMay env x of
+    Just x' -> return x'
+    Nothing -> error $ "Eval.eval': unbound reference: " ++ show x
+eval' _ (Global x) = do
   env <- get
-  lift $ lookup x env
-eval (App x y) = do
-  x' <- eval x
-  case x' of
-    Lam _ body -> eval $ substitute 0 y body
-    _          -> lift $ Nothing
-eval x          = return x
+  case lookup x env of
+    Just (_, x') -> return x'
+    Nothing      -> error $ "Eval.eval': unbound variable: " ++ x
+eval' env (Lam _ x) = return $ Closure env (Lam Nothing x)
+eval' env (App x y) = do
+  x' <- eval' env x
+  y' <- eval' env y
+  apply x' y'
+eval' _ (Num x)     = return $ Number x
+eval' _ (Bool x)    = return $ Boolean x
+eval' env (Let e x) = do
+  e' <- eval' env e
+  apply (Closure env (Lam Nothing x)) e'
 
-substitute :: Int -> Expr -> Expr -> Expr
-substitute i x (Ref i')
-  | i == i' = x
-  | i < i'  = Ref (i' - 1)
-substitute i x (Lam t body) = Lam t (substitute (i + 1) x body)
-substitute i x (App x' y')  = App (substitute i x x') (substitute i x y')
-substitute _ _ x            = x
+apply :: Value -> Value -> StateT Environment (Except String) Value
+apply (Closure env (Lam _ body)) x = eval' (x:env) body
+apply _ _                          = error $ "Eval.apply: not a closure"
